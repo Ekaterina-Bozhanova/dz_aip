73c7f9be3ed29c805c5eba9d91d209e50938cda6
Ekaterina-Bozhanova <79361548+Ekaterina-Bozhanova@users.noreply.github.com>
2021-03-24 00:44:40 +0900

    Add files via upload

---
+++ Aip 1_2/Aip2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+/*Пользователь вводит параметры фигуры. Найти периметр и площадь фигур: Прямоугольный треугольник*/
+int main()
+{
+    float a, b, S, P;
+    std::cout << "Enter side a ";
+    std::cin >> a;
+    std::cout << "Enter side b ";
+    std::cin >> b;
+    P = a + b + sqrt(a * a + b * b);
+    S = (a * b)/2;
+    std::cout << "S =" << S << std::endl;
+    std::cout << "P =" << P;
+
+}
+
