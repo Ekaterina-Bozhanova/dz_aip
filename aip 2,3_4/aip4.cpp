c2679093cbed83c4809a3d4ae136a0c715f92c2a
Ekaterina-Bozhanova <79361548+Ekaterina-Bozhanova@users.noreply.github.com>
2021-03-24 00:48:15 +0900

    Add files via upload

---
+++ aip 2,3_4/aip4.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+/*Вычислите значение выражения */
+
+int main()
+{
+	float x, Z;
+	std::cout << "Calculate the value of the expression:(abs(x - 5) - sin(x)) / 3 + sqrt(x * x + 2014) * cos(2 * x) - 3" << std::endl;
+	std::cout << "Enter x = ";
+	std::cin >> x;
+	Z = (abs(x - 5) - sin(x)) / 3 + sqrt(x * x + 2014) * cos(2 * x) - 3;
+	std::cout <<"Expression value = " <<Z;
+}
