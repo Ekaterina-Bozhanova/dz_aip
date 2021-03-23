b5092081e6c379d838a067593867ec15c88a5596
Ekaterina-Bozhanova <79361548+Ekaterina-Bozhanova@users.noreply.github.com>
2021-03-24 04:19:42 +0900

    Add files via upload

---
+++ aip 2,3_4/aip 2,3_4_file/aip4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <fstream>
+/*Вычислите значение выражения */
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	std::ofstream f("Answer.txt");
+
+	float x, Z;
+	std::cout << "Вычислите значение выражения :(abs(x - 5) - sin(x)) / 3 + sqrt(x * x + 2014) * cos(2 * x) - 3" << std::endl;
+	std::cout << "Введите  x = ";
+	std::cin >> x;
+	Z = (abs(x - 5) - sin(x)) / 3 + sqrt(x * x + 2014) * cos(2 * x) - 3;
+	std::cout <<"Значение выражения = " <<Z;
+
+	f << Z;
+	f.close();
+}
