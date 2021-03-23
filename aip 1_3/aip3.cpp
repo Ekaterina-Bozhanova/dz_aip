f1d31051e002d1b1365831af9868950ed184443b
Ekaterina-Bozhanova <79361548+Ekaterina-Bozhanova@users.noreply.github.com>
2021-03-24 00:45:48 +0900

    Add files via upload

---
+++ aip 1_3/aip3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+/*Пользователь вводит число. Выведите на экран квадрат этого числа, куб этого
+числа.*/
+
+int main()
+{
+    float a, a2, a3;
+    std::cout << "Enter the number ";
+    std::cin >> a;
+    a2 = a * a;
+    a3 = a * a * a;
+    std::cout << "Number squared " << a2 << std::endl;
+    std::cout <<"Number cube " << a3;
+
+}
+
+
