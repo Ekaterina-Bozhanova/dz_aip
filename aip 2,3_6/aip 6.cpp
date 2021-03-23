743d6a8f8d573a09ce2207d8cf72f8278b0fe104
Ekaterina-Bozhanova <79361548+Ekaterina-Bozhanova@users.noreply.github.com>
2021-03-24 01:16:16 +0900

    Add files via upload

---
+++ aip 2,3_6/aip 6.cpp
@@ -0,0 +1,18 @@
+﻿/* Пользователь вводит цены 1 кг конфет и 1 кг печенья. Найдите стоимость: а) одной
+покупки из 300 г конфет и 400 г печенья; б) трех покупок, каждая из 2 кг печенья и
+1 кг 800 г конфет.*/
+
+#include <iostream>
+
+int main()
+{
+    float x, y, s1, s2;
+    std::cout << "Enter the sum for 1 kg of sweets\n";
+    std::cin >> x;
+    std::cout << "Enter the sum for 1 kg of cookies\n";
+    std::cin >> y;
+    s1 = 0.3 * x + 0.4 * y;//sweets
+    s2 = 3 * (2 * x + 1.8 * y);//cookies
+    std::cout << "a)s="<<s1<< std::endl;
+    std::cout << "a)s="<<s2<< std::endl;
+}
