e5b8d1989d898000f615082a0614f4e6fe3f0167
Ekaterina-Bozhanova <79361548+Ekaterina-Bozhanova@users.noreply.github.com>
2021-03-24 04:18:59 +0900

    Add files via upload

---
+++ aip 1_3/aip 1_3_file/aip3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+
+/*Пользователь вводит число. Выведите на экран квадрат этого числа, куб этого
+числа.*/
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::ofstream f("Answer.txt");
+    
+    float a, a2, a3;
+    std::cout << "Введите число ";
+    std::cin >> a;
+    a2 = a * a;
+    a3 = a * a * a;
+    std::cout << "Число в квадрате " << a2 << std::endl;
+    std::cout <<"Число в кубе " << a3;
+
+    f << a2 << "   " << a3;
+    f.close();
+
+}
+
+
