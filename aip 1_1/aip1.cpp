8d96ef63f35ac99c5b3dd65b15baef985cfeef04
Ekaterina-Bozhanova <79361548+Ekaterina-Bozhanova@users.noreply.github.com>
2021-03-24 00:40:40 +0900

    Add files via upload

---
+++ aip 1_1/aip1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+/*Вывести на экран Первую букву имени из символов "*" (звездочка)*/
+
+int main()
+{
+    std::cout << "   **************"<<std::endl;
+    std::cout << "   *\n" ;
+    std::cout << "   *\n" ;
+    std::cout << "   *\n" ;
+    std::cout << "   *\n" ;
+    std::cout << "   *\n" ;
+    std::cout << "   **************\n";
+    std::cout << "   *\n" ;
+    std::cout << "   *\n" ;
+    std::cout << "   *\n" ;
+    std::cout << "   *\n" ;
+    std::cout << "   *\n" ;
+    std::cout << "   **************\n";
+
+}
