07c86f604a7073ff447828c7adf9d8e9e800e168
Ekaterina-Bozhanova <79361548+Ekaterina-Bozhanova@users.noreply.github.com>
2021-04-10 05:02:10 +0900

    Add files via upload

---
+++ aip5_5/aip5_5.cpp
@@ -0,0 +1,37 @@
+﻿/*
+Напишите программу, которая принимает от пользователя координаты точки и
+определяет, попала ли точка в заштрихованную область. 
+x ^ 2 + y ^ 2 = 1
+ x * x + y * y >= 0.3 
+*/
+
+
+#include <iostream>
+#include<math.h>
+
+int main(){
+
+    setlocale(LC_ALL, "Russian");
+
+    double x, y;
+    
+    std::cout << "Введите значение x ";
+    std::cin >> x;
+    std::cout << "Введите значение y ";
+    std::cin >> y;
+    if (y >= 0)
+        if (x <= 0)
+            if (sqrt(x * x + y * y) < 1)
+                std::cout << "Попала";
+            else
+                std::cout << "Не попала";
+        else
+            if (sqrt(x * x + y * y) < 1 && sqrt(x * x + y * y) > 0.3)
+                std::cout << "Попала";
+            else
+                std::cout << "Не попала";
+    else
+        std::cout << "Не попала";
+}
+
+
