6101f68652ab5e0daa91a7345c25e36520a68f84
Ekaterina-Bozhanova <79361548+Ekaterina-Bozhanova@users.noreply.github.com>
2021-06-04 01:25:02 +0900

    Add files via upload

---
+++ aip4_1/aip4_1.cpp
@@ -0,0 +1,21 @@
+﻿/*Вывести на экран Н строк из нулей, причем количество нулей в каждой строке
+равно номеру строки.Количество строк Н вводит пользователь(можно из файла).*/
+
+#include <iostream>
+
+int main()
+{
+    char a = 'H';
+    int count = 1;
+    int x;
+    std::cin >> x;
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < count; j++)
+            std::cout << a;
+        std::cout << std::endl;
+        count++;
+    }
+    return 0;
+}
+
